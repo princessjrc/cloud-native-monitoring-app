6c69c37d680f30416d2423ab89b1c07bc061bae1
Jordan Campbell <campbell.r.jordan@gmail.com>
2023-05-29 14:57:17 -0400

    edited ecr.py

--- ecr.py
+++ ecr.py
@@ -1,9 +1,23 @@
+# Import the boto3 library, which is the official AWS SDK for Python, and allows us to interact with AWS services.
+
 import boto3
+
+# Create an ECR client object for accessing the Amazon ECR service.
 
 ecr_client = boto3.client('ecr')
 
+# Specify the name of the ECR repository.
+
 repository_name = "my-cloud-native-repo"
+
+# Create a new ECR repository with the specified name.
+
 response = ecr_client.create_repository(repositoryName=repository_name)
 
+# Extract the repository URI from the response.
+
 repository_uri = response['repository']['repositoryUri']
+
+# Print the repository URI to the console.
+
 print(repository_uri)
